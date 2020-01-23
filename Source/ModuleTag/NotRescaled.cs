5fdcfaeb118fb4c889815fa4a5e4842ee4e0bef5
pehvbot <mshaslam@gmail.com>
2020-01-22 18:21:51 -0800

    Edit ModuleTag modules
    Remove unused tags and add ModuleTagNotRescaled for parts not scaled.

---
+++ Source/ModuleTag/NotRescaled.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LRTR.ModuleTags
+{
+    public class ModuleTagNotRescaled : ModuleTag
+    {
+        public override string GetInfo()
+        {
+            string str = string.Empty;
+            str = "This part has not been rescaled for Real Solar System.";
+            return str;
+        }
+    }
+}
